10bc48edd57395df980f47c23f749c34daaff438
Ankur Heble <=>
2023-10-07 14:56:38 +0530

    todo changes

---
+++ src/components/TodoList.js
@@ -0,0 +1,21 @@
+import React,{useState} from 'react'
+import TodoForm from './TodoForm'
+
+function TodoList() {
+    const [todo, setTodos] = useState([]);
+    
+    const addTodo = todo=>{
+        if(!todo.text)
+            return;
+
+        const newTodos = [todo,...todos];
+    }
+  return (
+    <div>
+        <h1>Whats on you mind</h1>
+        <TodoForm></TodoForm>
+    </div>
+  )
+}
+
+export default TodoList
